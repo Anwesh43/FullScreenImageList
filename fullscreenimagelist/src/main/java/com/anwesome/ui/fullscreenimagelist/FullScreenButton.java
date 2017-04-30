8b6b79b487f03bf9c6d41ce67025e36597da0635
Anwesh43 <anweshthecool0@gmail.com>
2017-04-30 08:46:49 +0530

    Using factor instead of dir

--- fullscreenimagelist/src/main/java/com/anwesome/ui/fullscreenimagelist/FullScreenButton.java
+++ fullscreenimagelist/src/main/java/com/anwesome/ui/fullscreenimagelist/FullScreenButton.java
@@ -9,10 +9,12 @@ import android.graphics.PointF;
  * Created by anweshmishra on 30/04/17.
  */
 public class FullScreenButton {
-    private float x,y,size,deg = 0,dir = 0;
+    private float x,y,size,deg = 0,initX,initY;
     public void setDimension(float x,float y,float size) {
         this.x = x;
         this.y = y;
+        this.initX = x;
+        this.initY = y;
         this.size = size;
     }
     public void draw(Canvas canvas, Paint paint) {
@@ -38,15 +40,10 @@ public class FullScreenButton {
         }
         canvas.restore();
     }
-    public void move() {
-        deg+=18*dir;
-        if(deg>90 || deg<0) {
-            dir = 0;
-            deg = (deg>90)?90:0;
-        }
-    }
-    public void startMoving() {
-        dir = deg<=0 ? 1 : -1;
+    public void move(float factor,float w,float h) {
+        deg = 90*factor;
+        x = initX+(w/2-initX)*factor;
+        y = initY+(h/2-initY)*factor;
     }
     public int hashCode() {
         return (int)(x+y);
