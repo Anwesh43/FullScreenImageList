32a9b5e1c43322bc6ea9bb7d3094f0530b790dd4
Anwesh43 <anweshthecool0@gmail.com>
2017-04-30 13:06:10 +0530

    Using the module in app

--- app/src/main/java/com/anwesome/ui/fullscreenimagelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/fullscreenimagelistdemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.anwesome.ui.fullscreenimagelistdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.fullscreenimagelist.FullScreenImageList;
 
+public class MainActivity extends AppCompatActivity {
+    private FullScreenImageList fullScreenImageList;
+    private int images[] = {R.drawable.back1,R.drawable.back2};
+    private Bitmap bitmaps[] = new Bitmap[images.length];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0;i<images.length;i++) {
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(),images[i]);
+        }
+        fullScreenImageList = new FullScreenImageList(this);
+        for(int i=0;i<images.length;i++) {
+            fullScreenImageList.addImage(bitmaps[i]);
+        }
+        fullScreenImageList.show();
+
+
 
     }
 }
